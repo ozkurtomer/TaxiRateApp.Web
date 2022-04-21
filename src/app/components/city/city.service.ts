cbe7890b21a85ab6f0d2fa1f606b7d2b9ad436d4
ozkurtomer <ozkurtomer@outlook.com>
2022-04-22 01:01:09 +0300

    create environment,  create post model and service

--- src/app/components/city/city.service.ts
+++ src/app/components/city/city.service.ts
@@ -3,16 +3,17 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Cities } from './city.model';
 import { ListResponseModel } from '../shared/base/model/responseModel/listResponseModel';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CityService {
-  apiUrl: string = 'https://localhost:44327/api/Cities/getall';
-
   constructor(private httpClient: HttpClient) {}
 
   getCities(): Observable<ListResponseModel<Cities>> {
-    return this.httpClient.get<ListResponseModel<Cities>>(this.apiUrl);
+    return this.httpClient.get<ListResponseModel<Cities>>(
+      environment.apiUrl + '/Cities/getall'
+    );
   }
 }

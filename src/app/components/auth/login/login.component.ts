af1c21622a17b418362227acb0a3934ee50d27a5
ozkurtomer <ozkurtomer@outlook.com>
2022-04-24 01:09:14 +0300

    login and register started. not completed

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import notify from 'devextreme/ui/notify';
+import { DxFormComponent } from 'devextreme-angular';
+import { LoginModel } from '../auth.model';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  isLoad = false;
+  saveButtonOptions: any = [];
+  formData: LoginModel;
+  password = '';
 
-  constructor() { }
+  @ViewChild(DxFormComponent, { static: false }) form: DxFormComponent;
 
-  ngOnInit(): void {
+  constructor(private authService: AuthService, private router: Router) {
+    this.saveButtonOptions = {
+      text: 'Login',
+      type: 'default',
+      stylingMode: 'outlined',
+      useSubmitBehavior: true,
+      onClick: function () {},
+    };
+    this.isLoad = true;
   }
 
+  ngOnInit(): void {}
+
+  passwordOptions: any = {
+    mode: 'password',
+    value: this.password,
+  };
+
+  buttonOptions: any = {
+    text: 'Register',
+    type: 'success',
+    useSubmitBehavior: true,
+  };
+
+  passwordComparison = () => this.form.instance.option('formData').Password;
+
+  checkComparison() {
+    return true;
+  }
+
+  onFormSubmit = function (e) {
+    this.authService.login(e.formData).subscribe();
+    notify(
+      {
+        message: 'You have submitted the form',
+        position: {
+          my: 'center top',
+          at: 'center top',
+        },
+      },
+      'success',
+      3000
+    );
+
+    e.preventDefault();
+  };
 }

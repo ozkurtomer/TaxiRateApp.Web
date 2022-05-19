2844375740e89b6c0a5006fe4bd1a8bf99db5ba1
ozkurtomer <ozkurtomer@outlook.com>
2022-05-19 22:01:24 +0300

    Fixed

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   isCollapsed = false;
-  constructor() {
+  constructor(private router: Router) {
     this.ngOnInit();
   }
   userName: string = '';
@@ -35,5 +36,6 @@ export class NavbarComponent implements OnInit {
   logOut(): void {
     localStorage.clear();
     window.location.reload();
+    this.router.navigate(['/']);
   }
 }
